8649dc85c7c35e801ab750c35fb641eccc1b7478
geronimo-lisboa <erikaeluciano2016@gmail.com>
2017-12-14 14:13:23 -0200

    Está acessando o banco

--- src/main/java/com/geronimo/don/SimpleReactCrud/TurmaController.java
+++ src/main/java/com/geronimo/don/SimpleReactCrud/TurmaController.java
@@ -1,13 +1,22 @@
 package com.geronimo.don.SimpleReactCrud;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TurmaController {
+	@Autowired
+	private TurmaRepository turmaRepo;
+	
+	//pra testar: curl -X GET localhost:8080/SimpleReactCrudApplication/app/turma
 	@RequestMapping(name="/app/turma", method=RequestMethod.GET)
 	public Turma getTurma() {
+		List<Turma> lista = turmaRepo.findAll();
+		System.out.println(lista);
 		Turma t = new Turma();
 		t.setId(1);
 		t.setNome("foobar");
