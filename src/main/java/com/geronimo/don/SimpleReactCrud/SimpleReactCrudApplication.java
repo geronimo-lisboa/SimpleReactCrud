b1becc19817ae404b3833d80cae1861c5a781015
geronimo-lisboa <erikaeluciano2016@gmail.com>
2017-12-14 09:56:13 -0200

    primeiro commit, só a estrutura básica do projeto

---
+++ src/main/java/com/geronimo/don/SimpleReactCrud/SimpleReactCrudApplication.java
@@ -0,0 +1,12 @@
+package com.geronimo.don.SimpleReactCrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleReactCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleReactCrudApplication.class, args);
+	}
+}
---
+++ src/test/java/com/geronimo/don/SimpleReactCrud/SimpleReactCrudApplicationTests.java
@@ -0,0 +1,16 @@
+package com.geronimo.don.SimpleReactCrud;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SimpleReactCrudApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
