c75926b564a9cb69900a7aef33a9404381739f3b
geronimo-lisboa <erikaeluciano2016@gmail.com>
2017-12-17 11:30:11 -0200

    Fetch do One-to-Many funcionando

--- src/main/java/com/geronimo/don/SimpleReactCrud/Detail.java
+++ src/main/java/com/geronimo/don/SimpleReactCrud/Detail.java
@@ -6,6 +6,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
@@ -24,6 +25,7 @@ public class Detail {
 	@NotNull
 	@JsonIgnore
 	@ManyToOne
+	@JoinColumn(name="fk_master")
 	private Master master;
 	
 	public Integer getId() {
